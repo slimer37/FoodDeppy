10c8bbd32b79299e22932ed449f5f05f35190a16
Alfred <59073967+slimer37@users.noreply.github.com>
2024-03-09 04:59:35 -0800

    bars

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as React from 'react';
+
+import { BarChart } from '@mui/x-charts/BarChart';
+
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mssLabels = ["Protein", "Carbohydrates",  "Fat"]
+  const mssData =   [-50,      10,              50]
+
+  let positiveMss = []
+  let negativeMss = []
+
+  mssData.forEach((val, index, _) => {
+    if (val > 0) {
+      positiveMss.push(mssData[index])
+      negativeMss.push(0)
+    } else {
+      negativeMss.push(mssData[index])
+      positiveMss.push(0)
+    }
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BarChart
+        colors={["blue", "red"]}
+        xAxis={[
+          {
+            id: 'barCategories',
+            data: mssLabels,
+            scaleType: 'band'
+          },
+        ]}
+        yAxis={[
+          {
+            label: 'Fulfillment %'
+          }
+        ]}
+        series={[
+          { data: positiveMss, stack: 'a' },
+          { data: negativeMss, stack: 'a' },
+        ]}
+        width={700}
+        height={500}
+      />
+    </ThemeProvider>
   )
 }
 
