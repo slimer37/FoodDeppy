62c35ab97a6a39e75cbe915f0e41a50cf2e36303
Alfred <59073967+slimer37@users.noreply.github.com>
2024-03-09 15:18:38 -0800

    replace friescanvas with temp merced data header

--- app/california/page.tsx
+++ app/california/page.tsx
@@ -7,21 +7,9 @@ const AnimatedPin = () => {
   return (
     <section className='py-36'>
       <div className='container flex flex-row items-center justify-center gap-20'>
-        <Card
-          isBlurred
-          className='max-w-[610px] border-none bg-background/60 dark:bg-default-100/50'
-          shadow='sm'
-        >
-          <CardHeader className='flex-col items-start px-4 pb-0 pt-2'>
-            <p className='text-large font-bold uppercase'>French Fries</p>
-            <small className='font-regular text-default-500'>McDonalds</small>
-          </CardHeader>
-          <CardBody className='overflow-visible py-2'>
-            <div className='mt-10 flex justify-center gap-6'>
-              <FriesCanvas />
-            </div>
-          </CardBody>
-        </Card>
+        <h3 className='!m-0 max-w-xs !pb-2 text-5xl font-bold text-[#1c1c84] dark:text-[#ffbf00]'>
+          Data for Merced, CA
+        </h3>
         <ThreeDCardDemo />
       </div>
     </section>
