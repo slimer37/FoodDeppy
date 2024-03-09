e172e020eed3e58d916667f2fa32e62a1733de22
davidhuang2003 <davidhuang1965@gmail.com>
2024-03-09 12:20:12 -0800

    pushing files to DavidH Branch

--- server/main.c
+++ server/main.c
@@ -1,14 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
 
-// TODO: Algorithms for relevant statistics, from CSVs
+#include "mss.h"
 
-int main(int argc, char **argv) {
-    
-    if (argc > 1) {
+#define NUM_CATEGORIES 3
+const char *ageCategories[NUM_CATEGORIES] = {"child", "teen", "adult"};
+
+int main(int argc, char **argv)
+{
+    // How to get csv file directory given value
+    if (argc > 1)
+    {
         printf("This is C. You entered: %s", argv[1]);
-    } else {
+    }
+    else
+    {
         printf("Not enough arguments.");
     }
-
     return 0;
+}
+
+typedef struct ageMacros
+{
+    char *category;
+    float macrosNeeded;
+} ageMacros;
+
+// checks if user input category is child, teen, adult
+bool check(char *word)
+{
+    printf("String Value: %s\n", word);
+    // if () //way to compare string values
+    // {
+    //     return true;
+    // }
+    return false;
+}
+
+// CSV Read function
+int totalMSS(char *fileInput, ageMacros *p)
+{
+    //1. Read the file columns 
+    //2. Compute the values 
+    //3. Output the value Protein: G, Carbs in G, Fat in G, 
+    //4.
+    char *buffer[1000];
+    FILE *file = fopen("data.csv", "r");
+    if (file != NULL)
+    {
+        while (fread("data.csv", "r", ))
+        {
+            fgets(buffer, sizeof(buffer), file);
+        }
+    }
+    return -1;
 }
---
+++ server/mss.h
@@ -0,0 +1,11 @@
+// MSS calculation header file
+#include <stdbool.h>
+
+#define LENGTH = 15 // define max length of chars for c
+
+int getTotalAmount();
+bool check(char *word);
+
+// CSV Reader files & computation
+int totalMSS(char *fileInput);
+int getServingSize();
