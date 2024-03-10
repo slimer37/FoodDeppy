dc0275d981c5958222e42fd91fb4cffd81d04211
Alfred <59073967+slimer37@users.noreply.github.com>
2024-03-09 21:27:04 -0800

    callback immediately so nutrient breakdown always showing

--- app/breakdown/NutrientDropdown.tsx
+++ app/breakdown/NutrientDropdown.tsx
@@ -3,6 +3,21 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function NutrientDropdown({value, callback} : any) {
+    const labels = ["Protein", "Carbohydrates", "Fat"]
+
+    value = useSearchParams().get('nutrient')
+
+    const [state, setState] = useState(null)
+
+    useEffect(() => {
+        for (let i = 0; i < labels.length; i++) {
+            if (labels[i] == value) {
+                callback(value)
+                break;
+            }
+        }
+    }, [state])
+
     return (
         <Dropdown>
             <DropdownTrigger>
